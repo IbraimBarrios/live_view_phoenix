081ac4362d407db128ecfa1b8e6c92515dea249f
ibrabarrios <barrios.party@hotmail.com>
2022-03-22 11:06:35 -0600

    page router and page index

--- lib/live_view_phoenix_web/router.ex
+++ lib/live_view_phoenix_web/router.ex
@@ -17,7 +17,7 @@ defmodule LiveViewPhoenixWeb.Router do
   scope "/", LiveViewPhoenixWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
+    get "/", PageController, :index #page router
   end
 
   # Other scopes may use custom stacks.
