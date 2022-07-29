44eaaf972543d4fb124d42d5f38c62ef0c6a17fb
Ibraim Barrios <barrios.party@hotmail.com>
2022-07-29 16:08:01 -0500

    feat:stateful and stateless components

--- lib/live_view_phoenix_web/live/sandbox_live.ex
+++ lib/live_view_phoenix_web/live/sandbox_live.ex
@@ -4,17 +4,30 @@ defmodule LiveViewPhoenixWeb.SandboxLive do
   # use Phoenix.LiveComponent
 
   alias LiveViewPhoenixWeb.QuoteComponent
+  alias LiveViewPhoenixWeb.SandboxCalculatorComponent
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, weight: nil, prece: nil)}
   end
 
   def render(assigns) do
     ~L"""
     <h1> Build a sandbox </h1>
     <div id="sandbox">
-        <%= live_component QuoteComponent, material: "sand" , weight: 10.0, price: 15.0 %>
+
+      <!--componentes con estado -->
+      <%= live_component @socket, SandboxCalculatorComponent, id: 1 %>
+
+      <!-- componente sin estado -->
+      <%= if @weight do %>
+        <%= live_component @socket, QuoteComponent, material: "sand", weight: @weight, price: @prece %>
+      <% end %>
     </div>
     """
+  end
+
+  def handle_info({:totals, weight, prece}, socket) do
+    socket = assign(socket, weight: weight, prece: prece)
+    {:noreply, socket}
   end
 end
